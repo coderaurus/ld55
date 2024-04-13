a827bd385e33989e3bd6fbe7b43eaa6c3e76df9f
CodeRaurus <coderaurus@gmail.com>
2024-04-13 21:18:01 +0300

    Add invocation prevention when cursor is not moving

--- scripts/Controls.gd
+++ scripts/Controls.gd
@@ -2,10 +2,16 @@ extends Node2D
 
 signal symbol_invoked
 
+var parent: Main
+
 func _ready():
+	parent = get_parent()
 	symbol_invoked.connect(get_parent().on_invoke)
 
 func _process(delta):
+	if not parent.level.cursor.moving:
+		return
+	
 	# MAP possible keys for summoning
 	if Input.is_action_just_pressed("summoning_space"):
 		symbol_invoked.emit("summoning_space")
