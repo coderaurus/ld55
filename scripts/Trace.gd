b0c84c7d3a1a6ad0a18d0698c858b66625e97eb0
CodeRaurus <coderaurus@gmail.com>
2024-04-14 19:38:19 +0300

    Change the trace length and how it retracts

--- scripts/Trace.gd
+++ scripts/Trace.gd
@@ -1,21 +1,21 @@
 extends Line2D
 
 var target: Node2D
+var stopped = false
 
 func setup(_target):
 	target = _target
+	stopped = false
 
 func stop():
-	var current_points = points
-	for i in current_points:
-		remove_point(0)
-		await get_tree().process_frame
+	stopped = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if points.size() == 360 or stopped:
+		remove_point(0)
+	
 	if target == null or (target is InvocationCursor and not (target as InvocationCursor).moving):
 		return
 	
-	if points.size() == 90:
-		remove_point(0)
 	add_point(target.global_position)
